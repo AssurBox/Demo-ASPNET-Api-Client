f3c3b14aa81863bce5a009db43aa3313c10b4d25
Sam Beauvois <samuel.beauvois@gmail.com>
2018-11-01 18:34:01 +0100

    test

--- Dealer simulation - ASP.NET/AssurBox.Samples.DealershipSimulator/AssurBox.Samples.Client.Garage.Web/Controllers/HomeController.cs
+++ Dealer simulation - ASP.NET/AssurBox.Samples.DealershipSimulator/AssurBox.Samples.Client.Garage.Web/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace AssurBox.Samples.Client.Garage.Web.Controllers
         [HttpPost]
         public async Task<ActionResult> Index(CredentialsModel model)
         {
-            // test 
+            // test f
             using (SDK.Clients.IdentityClient client = new SDK.Clients.IdentityClient(new SDK.AssurBoxClientOptions ()))
             {
                 var token = await client.GetBearerToken(model.ClientID, model.ClientSecret);
