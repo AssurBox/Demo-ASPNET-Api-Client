da378e2e3a73c4c86c59f85fe3c12ad38d74892b
Samuel Beauvois <samuel.beauvois@gmail.com>
2019-03-25 21:37:50 +0100

    #controller for coverage check

---
+++ Insurance simulation - ASP.NET/AssurBox.Samples.InsuranceEndPoint/AssurBox.Samples.API.Insurance/Controllers/CoverageCheckController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace AssurBox.Samples.API.Insurance.Controllers
+{
+    // ?risk=&identifiertype=&identifier=&cover=&cover=
+    public class CoverageCheckController : ApiController
+    {
+        public string Get(int risk, int identifierType, string identifier, int[] cover)
+        {
+            //todo : simulate errors for specific parameters
+            return "";
+        }
+
+
+    }
+}
